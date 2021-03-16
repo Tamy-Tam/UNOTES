5c577c7b6db95193561d2236e5178f2f84d64556
Tamara Robson <tamara.jade.robson@gmail.com>
2021-03-16 12:48:13 +0100

    Added controllers

---
+++ app/controllers/ycourses_controller.rb
@@ -0,0 +1,2 @@
+class YcoursesController < ApplicationController
+end
---
+++ test/controllers/ycourses_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class YcoursesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
