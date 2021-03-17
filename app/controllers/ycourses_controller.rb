96162138f1d730252920915194e81a62aae88019
Tamara Robson <tamara.jade.robson@gmail.com>
2021-03-17 12:50:29 +0100

    Ycourse ocntroller started

--- app/controllers/ycourses_controller.rb
+++ app/controllers/ycourses_controller.rb
@@ -1,2 +1,40 @@
 class YcoursesController < ApplicationController
+  before_action :set_ycourse, only: [:show, :edit, :update, :destroy]
+
+  def show
+    authorize @ycourse
+  end
+
+  def new
+    @ycourse = Ycourse.new
+    authorize @ycourse
+  end
+
+  def create
+    @ycourse = Ycourse.new(ycourse_params)
+    @ycourse.user_id = current_user.id
+    authorize @ycourse
+    if @ycourse.save
+      redirect_to note_path(@ycourse), notice: "A new Youtube Course has been added!"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    authorize @ycourse
+    @ycourse.destroy
+    redirect_to home_url, notice: 'Your Youtube Course was successfully destroyed'
+  end
+
+  private
+
+
+  def note_params
+    params.require(:ycourse).permit(:title, :url)
+  end
+
+  def set_ycourse
+    @ycourse = Ycourse.find(params[:id])
+  end
 end
