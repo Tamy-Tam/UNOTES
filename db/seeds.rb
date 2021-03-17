513b1d50d17776721b62f28459201467f7ebfad9
chris galet <galet.c@gmail.com>
2021-03-17 18:18:54 +0100

    seed notes ycourses ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,20 +59,8 @@ Ycourse6.save!
 puts 'ycourses created!'
 
 
-Ycourse4 = Ycourse.new(url: "https://www.youtube.com/embed/PE8FQ6zihhw",title: "Découverte du CSS (1/31) : Présentation")
-Ycourse4.save!
-
-Ycourse5 = Ycourse.new(url: "https://www.youtube.com/embed/J9w-cir5a6U",title: "CREER UN SITE ? HTML/CSS :1 - LES BASES")
-Ycourse5.save!
-
-Ycourse6 = Ycourse.new(url: "https://www.youtube.com/embed/0PA69L88HeI&list=PLjwdMgw5TTLV7VsXd9NOeq39soYXORezN",title: "NodeJS (1/6) : Qu’est ce que NodeJS ?")
-Ycourse6.save!
-
-puts 'ycourses created!'
-
-
-puts 'Creating 5 fake notes...'
-5.times do
+puts 'Creating 6 fake notes...'
+6.times do
   ycourse_one = Ycourse.last
   notes = Note.new(
     title: "Notes sur  #{Faker::Company.name}",
@@ -96,6 +84,6 @@ puts 'Creating 5 fake favourite notes...'
   fav_note.save!
 end
 puts 'favourite notes created!'
-=======
+
 puts 'Finished!'
-=======
+
