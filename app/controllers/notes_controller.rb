ef1e4044c7ef018d834cbb800637b0c1d1ebcede
Tamara Robson <tamara.jade.robson@gmail.com>
2021-03-17 11:45:41 +0100

    Note controller without create

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,50 @@
 class NotesController < ApplicationController
+skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @notes = Note.all
+    @notes = policy_scope(Note).order(created_at: :desc)
+  end
+
+  def show
+    authorize @note
+  end
+
+  def new
+    @note = Note.new
+    authorize @note
+  end
 
   def create
     authorize @note
+  end
+
+  def edit
+    authorize @note
+  end
+
+  def update
+    authorize @note
+    if @note.update(note_params)
+      redirect_to @note, notice: 'Your Note was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize @note
+    @note.destroy
+    redirect_to notes_url, notice: 'Your Note was successfully destroyed'
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title)
+  end
+
+  def set_note
+    @note = Note.find(params[:id])
   end
 end
