27285a953ffbfd67ca9a83a77fd7cb701d5c7add
Tamara Robson <tamara.jade.robson@gmail.com>
2021-03-18 18:52:45 +0100

    Favouritenotes contoller started part 2

--- app/controllers/favourite_notes_controller.rb
+++ app/controllers/favourite_notes_controller.rb
@@ -5,67 +5,46 @@ before_action :set_favouritenote, only: [:show, :edit, :update, :destroy]
   def index
     @favourite_notes = Favourite_note.all || "0"
     @current_user = current_user
-    @ycourses_url = @ycourses.map do |ycourse|
-      get_youtube_id(ycourse.url)
-    end
-    @fav_notes = Favourite_note.all || "0"
-
-    if params[:search].present?
-      @search_favourite_notes = FavouriteNote.where("content ILIKE ?", "%#{params[:search]}%")
-      @search_ycourse = @search_favourite_notes.map do |ycourse|
-        get_youtube_id(Ycourse.find(ycourse.ycourse_id).url)
-      end
-    else
-      @search_favourite_notes = @favourite_note
-    end
   end
 
   def show
-    authorize @favourite_note
-    @ycourse = Ycourse.find(params[:id]) || "0"
-    @ycourseID = get_youtube_id(@ycourse.url)
   end
 
   def new
     @favourite_note = Favourite_note.new
-    @ycourse = Ycourse.new
-    @favourite_note.ycourse = @ycourse
-    authorize @favourite_note
+    @note = @favourite_note.note
   end
 
   def create
-    @favourite_note = Favourite_note.new(note_params)
+    @favourite_note = Favourite_note.new(favourite_note_params)
     @notes = @favourite_note.note
     @favourite_note.user_id = current_user.id
     if @favourite_note.save
-      redirect_to note_path(@favourite_note), notice: "A new Favourite Note has been created!"
+      redirect_to note_path(@note), notice: "A new Favourite Note has been created!"
     else
       render :new
     end
   end
 
   def edit
-    authorize @favourite_note
   end
 
   def update
-    authorize @favourite_note
-    if @favourite_note.update(note_params)
-      redirect_to @favourite_note, notice: 'Your Favourite Note was successfully updated'
+    if @favourite_note.update(favourite_note_params)
+      redirect_to @note, notice: 'Your Favourite Note was successfully updated'
     else
       render :edit
     end
   end
 
   def destroy
-    authorize @favourite_note
     @favourite_note.destroy
     redirect_to notes_url, notice: 'Your FavouriteNote was successfully destroyed'
   end
 
   private
 
-  def note_params
+  def favourite_note_params
     params.require(:FavouriteNote).permit()
   end
 
