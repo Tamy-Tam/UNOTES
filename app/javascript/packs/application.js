17393895e9da84956aa4968f73ab325e5522be74
chris galet <galet.c@gmail.com>
2021-03-18 16:23:16 +0100

    Editor-fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,12 +26,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import "@editorjs/editorjs"
+
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { initEditor } from '../plugins/editor_js_editor';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initEditor();
 });
