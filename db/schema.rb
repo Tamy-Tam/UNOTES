187b3a8a19d28e9b8d8d5e64bf1882c483f3a664
chris galet <galet.c@gmail.com>
2021-03-24 15:44:04 +0100

    fix search

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_133026) do
+ActiveRecord::Schema.define(version: 2021_03_24_095940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2021_03_22_133026) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.jsonb "json_content", default: "{}", null: false
+    t.text "text_content"
+    t.index ["text_content"], name: "index_notes_on_text_content"
     t.index ["user_id"], name: "index_notes_on_user_id"
     t.index ["ycourse_id"], name: "index_notes_on_ycourse_id"
   end
